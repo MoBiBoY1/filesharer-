bf83bd727cd9f7886884a2a6a7d6342db65928af
MoBiBoY1 <115732041+MoBiBoY1@users.noreply.github.com>
2023-04-09 21:16:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>➰️ This is an Meme Bot That Provides Memes To You.\n\n○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>➰️ 𝐓𝐡𝐢𝐬 𝐢𝐬 𝐓𝐡𝐞 𝐌𝐞𝐦𝐞 𝐁𝐨𝐭 𝐓𝐡𝐚𝐭 𝐏𝐫𝐨𝐯𝐢𝐝𝐞𝐬 𝐃𝐚𝐢𝐥𝐲 𝐌𝐞𝐦𝐞𝐬😂 𝐓𝐨 𝐘𝐨𝐮 | Jᴏɪɴ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ @Loki_BoTs\n\n○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MoBiBoY1'>Click here</a>\n○ Channel : @Loki_BoTs\n○ leech groups: @loki_leech_updates</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
