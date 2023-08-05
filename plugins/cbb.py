c4464017192626145c858b5cc36bab3a42d5665a
MoBiBoY1 <115732041+MoBiBoY1@users.noreply.github.com>
2023-08-05 20:12:22 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>➰️ Bot is made for sharing desi/Indian videos via special link | Jᴏɪɴ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ @Loki_BoTs\n\n○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MoBiBoY1'>Click here</a>\n○ Channel : @Loki_BoTs\n○ leech groups: @loki_leech_updates</b>",
+            text = f"<b>➰️ Bot is made for sharing desi/Indian videos via special link | Jᴏɪɴ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ @Loki_BoTs\n\n○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegra.ph/file/71bf9c38119565125fec2.png'>Click here</a>\n○ Channel : @Loki_BoTs\n○ </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
